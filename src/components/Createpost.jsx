7f22fe99407fdcfe406d4271041dbd173e6f3118
NAQVI <nomanalam7214@gmail.com>
2019-08-09 09:30:30 +0200

    created  createpost component

---
+++ src/components/Createpost.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Createpost extends Component {
+    render() {
+        return (
+            <div>
+                create post form
+            </div>
+        )
+    }
+}
