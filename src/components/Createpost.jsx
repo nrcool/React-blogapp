7aca1d41ef87063de6f53395b16953d2c4299bbc
NAQVI <nomanalam7214@gmail.com>
2019-08-09 22:51:47 +0200

    added upload image function

--- src/components/Createpost.jsx
+++ src/components/Createpost.jsx
@@ -1,28 +1,36 @@
 import React, { Component } from 'react'
 
 export default class Createpost extends Component {
-    render() {
-        return (
-            <form>
-            <div className="form-group row">
-              <label className="col-sm-2 col-form-label">UserName :</label>
-              <div className="col-sm-10">
-                <input type="text" className="form-control" placeholder="UserName"/>
-              </div>
-            </div>
-            <div className="form-group row">
-              <label className="col-sm-2 col-form-label">Title :</label>
-              <div className="col-sm-10">
-                <input type="text" className="form-control" placeholder="Title"/>
-              </div>
-            </div> <div className="form-group row">
-              <label className="col-sm-2 col-form-label">Content :</label>
-              <div className="col-sm-10">
-                <input type="text" className="form-control" placeholder="POST"/>
-              </div>
-            </div>
-            <button type="button" class="btn btn-success">Create a Post</button>
-          </form>
-        )
-    }
+
+  render() {
+    return (
+      <form onSubmit={this.props.submitform} >
+        <div className="form-group row">
+          <label className="col-sm-2 col-form-label" >UserName :</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" placeholder="UserName" value={this.props.username} onChange={this.props.getusername} />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label className="col-sm-2 col-form-label">Title :</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" placeholder="Title" value={this.props.title} onChange={this.props.gettitle} />
+          </div>
+        </div> 
+        <div className="form-group row">
+          <label className="col-sm-2 col-form-label">Content :</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" placeholder="POST" value={this.props.content} onChange={this.props.getcontent} />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label className="col-sm-2 col-form-label">upload image :</label>
+          <div className="col-sm-10">
+            <input type="file" className="form-control" placeholder="image" onChange={this.props.uploadimage} />
+          </div>
+        </div>
+        <button type="submit" className="btn btn-success">Create a Post</button>
+      </form>
+    )
+  }
 }
--- src/components/ShowallPosts.jsx
+++ src/components/ShowallPosts.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
-export default function ShowallPosts() {
+export default function ShowallPosts(props) {
+    console.log(props)
     return (
-        <div className="jumbotron">
-        <h2 className="text-center text-bold bg-success">Hello, world!</h2>
-        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr/>
-        <p>{new Date().toLocaleTimeString()}</p>
-      </div>
+       <div>
+           {props.all.map((item,index)=>{
+               return (
+                <div className="container alert-dark" key={index}>
+                    <h2 className="text-center text-bold bg-success">{item.title}<button className="float-right deletebtn">X</button></h2>
+                <details> 
+                <summary>Read full article ,click here</summary>
+                    {item.file?(<img src={item.file} className="rounded mx-auto d-block" width="300" alt="myimage"/>):""}
+                    <p className="lead text-center p-2">{item.content}</p>
+                </details>
+                <hr/>
+                <p className="p-2 m-2">{item.username} Wrote at: {item.time}</p>
+              </div>
+               )
+           })}
+       </div>
     )
 }
