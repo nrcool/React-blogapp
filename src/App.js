d148a7021ad698c81b780364f0811dab60e4ed06
NAQVI <nomanalam7214@gmail.com>
2019-08-09 22:52:01 +0200

    added upload image functiion

--- src/App.js
+++ src/App.js
@@ -6,19 +6,87 @@ import ShowallPosts from './components/ShowallPosts';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 
-function App() {
-  return (
+class App extends React.Component{
+  state={
+    username:"",
+    title:"",
+    content:"",
+    Posts:[],
+    file:null
+}
+getusername=(e)=>{
+    this.setState({
+        username:e.target.value
+    })
+}
+gettitle=(e)=>{
+    this.setState({
+        title:e.target.value
+    })
+}
+getcontent=(e)=>{
+    this.setState({
+        content:e.target.value
+    })
+}
+submitform=(e)=>{
+    e.preventDefault();
+    let post={
+        username:this.state.username,
+        title:this.state.title,
+        content:this.state.content,
+        file:this.state.file,
+        time:new Date().toLocaleTimeString()
+    }
+    let allposts=this.state.Posts;
+    allposts.push(post);
+    this.setState({
+        Posts:allposts,
+        username:"",
+        title:"",
+        content:"",
+
+    })
+    console.log(this.state)
+}
+uploadimage=(event)=>{ 
+    this.setState({
+      file: URL.createObjectURL(event.target.files[0])
+    })
+}
+  render(){
+  
+    return (
     <div className="App container">
       <Router>
       <Navbar/>
       <Switch>
        <Route exact path="/" component={Home}/> 
-       <Route path="/createpost" component={Createpost}/>
-       <Route path="/showallposts" component={ShowallPosts}/>
+       <Route path="/createpost"
+        render={()=>{
+          return( <Createpost getcontent={this.getcontent}
+          gettitle={this.gettitle}
+          getusername={this.getusername}
+          submitform={this.submitform}
+          username={this.state.username}
+          title={this.state.title}
+          content={this.state.content}
+          uploadimage={this.uploadimage}
+          />)}
+       
+      }
+        />
+       <Route path="/showallposts" render={()=>{
+         return(
+           <ShowallPosts all={this.state.Posts}/>
+         )
+       }}/>
        </Switch>
        </Router>
     </div>
   );
+  }
+  
 }
 
 export default App;
