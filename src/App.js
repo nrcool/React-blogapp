90725cfca2cc8a881ed060a5e63a41a5a63b391f
NAQVI <nomanalam7214@gmail.com>
2019-08-09 10:36:53 +0200

    imported react-router-dom

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import Home from './components/Home';
 import Createpost from './components/Createpost';
 import ShowallPosts from './components/ShowallPosts';
 import Navbar from './components/Navbar';
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
+      <Router>
       <Navbar/>
-       <Home/>
-       <Createpost/>
-       <ShowallPosts/>
+      <Switch>
+       <Route exact path="/" component={Home}/> 
+       <Route path="/createpost" component={Createpost}/>
+       <Route path="/showallposts" component={ShowallPosts}/>
+       </Switch>
+       </Router>
     </div>
   );
 }
