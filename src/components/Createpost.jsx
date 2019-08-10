77c9cfa9df9dc77a32453afccc3b1e91fe7cf144
NAQVI <nomanalam7214@gmail.com>
2019-08-10 11:58:58 +0200

    update item time on edit

--- src/components/Createpost.jsx
+++ src/components/Createpost.jsx
@@ -8,19 +8,19 @@ export default class Createpost extends Component {
         <div className="form-group row">
           <label className="col-sm-2 col-form-label" >UserName :</label>
           <div className="col-sm-10">
-            <input type="text" className="form-control" placeholder="UserName" value={this.props.username} onChange={this.props.getusername} />
+            <input type="text" required={true } className="form-control" placeholder="UserName" value={this.props.username} onChange={this.props.getusername} />
           </div>
         </div>
         <div className="form-group row">
           <label className="col-sm-2 col-form-label">Title :</label>
           <div className="col-sm-10">
-            <input type="text" className="form-control" placeholder="Title" value={this.props.title} onChange={this.props.gettitle} />
+            <input type="text" required={true } className="form-control" placeholder="Title" value={this.props.title} onChange={this.props.gettitle} />
           </div>
         </div> 
         <div className="form-group row">
           <label className="col-sm-2 col-form-label">Content :</label>
           <div className="col-sm-10">
-            <input type="text" className="form-control" placeholder="POST" value={this.props.content} onChange={this.props.getcontent} />
+            <input type="text" required={true } className="form-control" placeholder="POST" value={this.props.content} onChange={this.props.getcontent} />
           </div>
         </div>
         <div className="form-group row">
