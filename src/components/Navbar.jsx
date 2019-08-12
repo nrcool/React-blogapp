0edd3018b802463d9f36a95d4bb90800a74dfe8d
NAQVI <nomanalam7214@gmail.com>
2019-08-12 10:19:19 +0200

    deleted collapse from navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,6 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <Link className="navbar-brand" to="/">BLOG</Link>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav">
       <li className="nav-item active">
         <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
@@ -20,7 +16,6 @@ export default function Navbar() {
         <Link className="nav-link" to="/showallposts">Show All Post</Link>
       </li>
     </ul>
-  </div>
 </nav>
     )
 }
