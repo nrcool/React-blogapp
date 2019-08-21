64363c031cf09ce715bbbd1fee4c4544191b2191
NAQVI <nomanalam7214@gmail.com>
2019-08-21 19:25:56 +0200

    Updates

---
+++ precache-manifest.5771dceb86ca58a356af0710f8803e9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bdf200253a58a9dde0a4a77d7ec824d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d9b1ef83d4640cd29711",
+    "url": "./static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c83fa3c10114495f2b54",
+    "url": "./static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "d9b1ef83d4640cd29711",
+    "url": "./static/js/2.a55195ea.chunk.js"
+  },
+  {
+    "revision": "c83fa3c10114495f2b54",
+    "url": "./static/js/main.06b25f44.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
